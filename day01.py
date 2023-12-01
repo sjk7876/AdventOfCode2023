142dd0fbece86146b88268a630bd5f596b78a1f3
sjk7876 <sjk7876@rit.edu>
2023-12-01 16:22:45 -0500

    day 1 part 1

--- day01.py
+++ day01.py
@@ -2,8 +2,20 @@
 Solution for Day 1 of Advent Of Code 2023
 '''
 
-def main():
-    print("Hello World")
+def getNum(s, direction):
+    for letter in s[::direction]:
+        if letter.isdigit():
+            return letter
+    return "0"
 
+
+def main():
+    total = 0
+
+    with open("01.txt", "r") as f:
+        for s in f:
+            total += int("" + getNum(s, 1) + getNum(s, -1))    
+
+    print(total)
 
 main()
