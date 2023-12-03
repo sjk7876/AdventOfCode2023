c65844611d77aa44a8ab357e71725c493628e12a
sjk7876 <sjk7876@rit.edu>
2023-12-03 01:13:29 -0500

    created base program for each day

---
+++ base.py
@@ -0,0 +1,13 @@
+def partOne():
+    with open("02.txt", "r") as f:
+        for s in f:
+            print()
+
+
+def partTwo():
+    with open("02.txt", "r") as f:
+        print()
+    
+
+partOne()
+partTwo()
