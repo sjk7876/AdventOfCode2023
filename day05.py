d36ce929d5479335f2797064d73b077cc449969f
sjk7876 <sjk7876@rit.edu>
2023-12-06 21:31:33 -0500

    day 5 gave up on part 2

--- day05.py
+++ day05.py
@@ -23,27 +23,42 @@ def partOneHelper(seeds, maps):
 	results = []
 	for seed in seeds:
 		current = seed
-		nex = current
 
 		for i in range(len(maps)): # each mapping (seed to soil)
 			for j in range(len(maps[i])): # specifics (50 98 2)
 				if current >= maps[i][j][1] and current < maps[i][j][1] + maps[i][j][2]:
-					nex = (current - maps[i][j][1]) + maps[i][j][0]
-					debugPrint('in', current, nex)
+					current = (current - maps[i][j][1]) + maps[i][j][0]
 					break
-				debugPrint('in', current, nex)
-			debugPrint('out', current, nex, '\n')
-			current = nex
+
 		results.append(current)
 	
 	return min(results)
 
 
 def partTwo(fi):
-	with open(fi, "r") as f:
-		for s in f:
-			print(end='')
-	
+	with open(fi, "r") as f: 
+		rangesList = [[], [], [], [], [], [], []]
+
+		seedsPre = [int(n) for n in f.readline().rstrip().split(': ')[1].split()]
+		seeds = []
+
+		for i in range(0, len(seedsPre) - 1, 2):
+			seeds.extend([seedsPre[i] + j for j in range(seedsPre[i + 1])])
+		# print(seeds)
+
+		f.readline()
+		for i in range(7):
+			f.readline()
+
+			while True:
+				line = f.readline().rstrip()
+				if not line:
+					break
+				line = [int(n) for n in line.split()]
+				rangesList[i].append(line)
+			
+	print(partOneHelper(seeds, rangesList))
+
 
 def main():
 	if TESTING:
