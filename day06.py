91d763c253b6f2e47c342e6b8a4dc9ab1786a6ae
sjk7876 <sjk7876@rit.edu>
2023-12-06 21:31:25 -0500

    day 6 part 1

---
+++ day06.py
@@ -0,0 +1,47 @@
+DEBUG_PRINTING = True
+TESTING = False
+
+from math import prod
+
+def partOne(fi):
+	with open(fi, "r") as f:
+		times = [int(n) for n in ' '.join(f.readline().rstrip().split()).split(': ')[1].split(' ')]
+		distances = [int(n) for n in ' '.join(f.readline().rstrip().split()).split(': ')[1].split(' ')]
+	
+	print(partOneHelper(times, distances))
+
+
+def partOneHelper(t, d):
+	waysToBeat = [0] * len(t)
+	for i in range(len(t)):
+		currentSpeed = 0
+		for j in range(t[i]):
+			if d[i] < (currentSpeed * (t[i] - j)):
+				waysToBeat[i] += 1
+			currentSpeed += 1
+	
+	return prod(waysToBeat)
+
+
+
+def partTwo(fi):
+	with open(fi, "r") as f:
+		for s in f:
+			print(end='')
+	
+
+def main():
+	if TESTING:
+		fi = "test.txt"
+	else:
+		fi = "06.txt"
+	
+	partOne(fi)
+	partTwo(fi)
+	
+
+def debugPrint(*s):
+	if DEBUG_PRINTING:
+		print(s)
+
+main()
